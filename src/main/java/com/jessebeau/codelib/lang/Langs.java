b45007d73b611abf74829695d9d4dccc1f5fed38
Jessebeau2001 <jessegamen@gmail.com>
2026-01-18 20:48:21 +0100

    Added lang example code

--- src/main/java/com/jessebeau/codelib/lang/Langs.java
+++ src/main/java/com/jessebeau/codelib/lang/Langs.java
@@ -1,5 +1,6 @@
 package com.jessebeau.codelib.lang;
 
+import java.util.List;
 import java.util.Map;
 
 public final class Langs {
@@ -8,5 +9,16 @@ public final class Langs {
 
 	public static LangDictionary ofEntries(Map<String, Resolvable> entries, Resolver resolver) {
 		return new LangDictionary(Map.copyOf(entries), resolver);
+	}
+
+	public static void main(String[] args) {
+		// var lang = Langs.fromResource("/lang/en-US.json", Resolver.createLoose());
+		Map<String, Resolvable> mappings = Map.of(
+				"example.hello", new LangEntry("Hello, world!"),
+				"example.greet", new LangEntry("Hello {name}!", List.of("name"))
+		);
+		LangDictionary lang = Langs.ofEntries(mappings, Resolver.createStrict());
+		System.out.println(lang.transcribe("example.hello"));
+		System.out.println(lang.transcribe("example.greet", 67));
 	}
 }
